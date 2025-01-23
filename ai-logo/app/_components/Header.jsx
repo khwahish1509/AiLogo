89b46ed9da91ba5cf7977f0170317743cb63a7f7
Khwahish <khwahishvaid07@gmail.com>
2025-01-23 00:32:03 -0500

    Header

--- ai-logo/app/_components/Header.jsx
+++ ai-logo/app/_components/Header.jsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <div>Hello Kv!</div>
-        <Button>Click</Button>
+    <div className='px-10 lg:px-32 xl:px-48 2xl:px-56 p-4 flex justify-between items-center shadow-sm'>
+        <Image src={'/logo.svg'} alt='logo' width={50} height={50}/>
+        <Button>Get Started</Button>
     </div>
   )
 }
