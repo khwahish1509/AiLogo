0e3681f0d3e19da6da4bc99848253533ecd8f694
Khwahish <khwahishvaid07@gmail.com>
2025-01-23 16:00:25 -0500

    use state

--- ai-logo/app/create/_component/LogoTitle.jsx
+++ ai-logo/app/create/_component/LogoTitle.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+"use client"
+import React , {useState} from 'react'
 import HeadingDescription from './HeadingDescription'
 import Lookup from '@/app/_data/Lookup'
+import { useSearchParams } from 'next/navigation'
 
-function LogoTitle() {
+function LogoTitle({onHandleInputChange}) {
+    const searchParam=useSearchParams();
+    const [title, setTitle] = useState(searchParam?.get('title')??'')
   return (
     <div className='my-10'>
         <HeadingDescription title={Lookup?.LogoTitle}
         description={Lookup.LogoTitleDesc}/>
+
+        <input type="text" placeholder={Lookup.InputTitlePlaceholder}
+        className='p-4 border rounded-lg mt-5 w-full'
+        defaultValue={title} 
+        onChange={(e)=>onHandleInputChange(e.target.value)} />
     </div>
   )
 }
--- ai-logo/app/create/page.jsx
+++ ai-logo/app/create/page.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LogoTitle from './_component/LogoTitle'
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/20/solid';
+
 
 function CreateLogo() {
+    const [step, setStep] = useState(1);
+    const onHandleInputChange=()=>{
+
+    }
   return (
     <div className='mt-28 p-10 border rounded-xl 2xl:mx-72'>
-        <LogoTitle/>
+        {step==1?
+        <LogoTitle onHandleInputChange={(v)=>onHandleInputChange('title',v)}/>:
+        null
+        }
+
+        <div className='flex items-center justify-between mt-10'>
+            <Button variant="outline"> <ArrowLeftIcon/> Previous</Button>
+            <Button> <ArrowRightIcon/> Continue</Button>
+        </div>
     </div>
   )
 }
