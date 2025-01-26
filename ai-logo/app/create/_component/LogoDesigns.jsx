2040e6a87594a8d5e4e6f1e9ecb5dbaf5fe5407e
Khwahish <khwahishvaid07@gmail.com>
2025-01-25 19:00:30 -0500

    images added

--- ai-logo/app/create/_component/LogoDesigns.jsx
+++ ai-logo/app/create/_component/LogoDesigns.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import HeadingDescription from './HeadingDescription'
+import Lookup from '@/app/_data/Lookup'
+import LogoDesig from '@/app/_data/LogoDesig'
+import Image from 'next/image'
 
 function LogoDesigns() {
   return (
-    <div>LogoDesigns</div>
+    <div className='my-10'>
+      <HeadingDescription
+      title={Lookup.LogoDesignTitle}
+      description={Lookup.LogoDesignDesc}
+      />
+
+      <div>
+        {LogoDesig.map((design, index)=>
+         <div key={index}>
+            <Image src={design.image} alt={design.title} width={300}
+             height={200}>
+
+            </Image>
+         </div>
+        )}
+      </div>
+    </div>
   )
 }
 
