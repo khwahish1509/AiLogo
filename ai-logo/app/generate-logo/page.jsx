d1a8ed108d6a2ccec0da615e822351e4f13c66e3
Khwahish <khwahishvaid07@gmail.com>
2025-01-30 13:52:24 -0500

    successfully getting prompt for generating logo

--- ai-logo/app/generate-logo/page.jsx
+++ ai-logo/app/generate-logo/page.jsx
@@ -1,6 +1,70 @@
-import React from 'react'
+"use client"
+import React, { useContext, useEffect, useState } from 'react'
+import { UserDetailContex } from '../_context/UserDetailContext';
+import Prompt from '../_data/Prompt';
+import axios from 'axios';
+
 
 function GenerateLogo() {
+  const {userDetail,setUserDetail}=useContext(UserDetailContex);
+  const [formData,setFormData]=useState();
+
+
+
+  useEffect(()=>{
+    if(typeof window !=undefined && userDetail?.email)
+    {
+        const storage=localStorage.getItem('formData')
+        if(storage)
+        {
+            setFormData(JSON.parse(storage));
+            console.log(JSON.parse(storage))
+        }
+    }
+  },[userDetail])
+
+  useEffect(() => {
+    if (formData?.title) {
+      GenerateAILogo();
+    }
+  }, [formData])
+
+  const GenerateAILogo=async()=>{
+
+    // if(modelType!='Free'&&userDetail?.credits<=0)
+    // {
+    //     console.log("Not Enough Credits")
+    //     toast('Not enought credits!!!')
+    //     return ;
+    // }
+
+    // setLoading(true);
+    const PROMPT=Prompt.LOGO_PROMPT
+    .replace('{logoTitle}',formData?.title)
+    .replace('{logoDesc}',formData?.desc)
+    .replace('{logoColor}',formData.palette)
+    .replace('{logoIdea}',formData?.idea)
+    .replace('{logoDesign}',formData?.designs?.title)
+    .replace('{logoPrompt}',formData?.designs?.prompt);
+
+    console.log(PROMPT);
+
+    // //Generate Logo Prompt from AI
+    // //Generate Logo Image
+    // const result=await axios.post('/api/ai-logo-model',{
+    //   prompt:PROMPT,
+    //   email:userDetail?.email,
+    //   title:formData.title,
+    //   desc:formData.desc,
+    //   type:modelType,
+    //   userCredits:userDetail?.credits
+    // });
+   
+    // setLogoImage(result.data?.image)
+    // setLoading(false);
+   
+  }
+
   return (
     <div>GenerateLogo</div>
   )
