dee194268b6daa2378acbe61111338afd729659f
Khwahish <khwahishvaid07@gmail.com>
2025-02-03 17:02:59 -0500

    Done with app

--- ai-logo/app/dashboard/_components/LogoList.jsx
+++ ai-logo/app/dashboard/_components/LogoList.jsx
@@ -31,13 +31,17 @@ function LogoList() {
   return (
     <div className='mt-10'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+            {/* // Display logoList if it is not empty, else display 6 loading placeholders */}
             {logoList?.length>0?logoList.map((logo,index)=>(
                 <div key={index} className='hover:scale-105 transition-all cursor-pointer'
                 onClick={()=>ViewLogo(logo?.image)}
                 >
                     <Image src={logo?.image} width={400} height={200}
                     className='w-full rounded-xl'
+
                     alt={logo?.title}
+
+
                     />
                     <h2 className='text-center text-lg font-medium mt-2'>{logo?.title}</h2>
                     <p className='text-sm text-gray-500 text-center'>{logo?.desc}</p>
