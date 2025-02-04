5b26e51e10659be071026ac07d3e66b9987df140
Khwahish <khwahishvaid07@gmail.com>
2025-02-04 14:56:33 -0500

    deploy error

--- ai-logo/app/dashboard/page.jsx
+++ ai-logo/app/dashboard/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 import React from 'react'
 // import Info from './_components/Info'
+import { UserDetailContext } from "../provider";  // ✅ Import Provider
 import LogoList from './_components/LogoList'
 import Info from './_components/Info'
 
 function Dashboard() {
+  const { userDetail } = useContext(UserDetailContext) || {};
   return (
+    
     <div className='mt-20'>
         <Info/>
 
